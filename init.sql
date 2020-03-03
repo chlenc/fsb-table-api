ac4fc3418013415ce1b813307e0cb2fb020655f4
Alexey Nagorny <anagorny@wavesplatform.com>
2020-03-03 12:06:54 +0300

    done :rocket:

---
+++ init.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users
+(
+    id serial PRIMARY KEY,
+    firstname TEXT,
+    lastname TEXT,
+    email TEXT,
+    phone TEXT,
+    address TEXT,
+    description TEXT
+);
+
+INSERT INTO users
+VALUES
+  (1, 'user1', 'user1', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (2, 'user2', 'user2', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (3, 'user3', 'user3', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (4, 'user4', 'user4', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (5, 'user5', 'user5', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (6, 'user6', 'user6', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (7, 'user7', 'user7', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (8, 'user8', 'user8', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (9, 'user9', 'user9', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (10, 'user10', 'user10', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (11, 'user11', 'user11', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (12, 'user12', 'user12', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (13, 'user13', 'user13', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (14, 'user14', 'user14', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (15, 'user15', 'user15', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (16, 'user16', 'user16', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (17, 'user17', 'user17', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (18, 'user18', 'user18', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (19, 'user19', 'user19', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (20, 'user20', 'user20', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (21, 'user21', 'user21', 'email', '89000000000', 'address', 'lorem ipsum'),
+  (22, 'user22', 'user22', 'email', '89000000000', 'address', 'lorem ipsum');
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type App struct {
+	db *gorm.DB
+}
+
+type User struct {
+	Id          int    `sql:"AUTO_INCREMENT" gorm:"primary_key"`
+	Firstname   string `sql:"null"`
+	Lastname    string `sql:"null"`
+	Email       string `sql:"null"`
+	Phone       string `sql:"null"`
+	Address     string `sql:"null"`
+	Description string `sql:"null"`
+}
+
+func (i *User) TableName() string {
+	return "users"
+}
+
+func main() {
+
+	router, db := initializeAPI()
+	defer db.Close()
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func initializeAPI() (*gin.Engine, *gorm.DB) {
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=template1 sslmode=disable")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var app = App{db}
+
+	r := gin.Default()
+	app.initializeRoutes(r)
+
+	r.Run()
+
+	return r, db
+}
+
+func (app *App) initializeRoutes(r *gin.Engine) {
+	r.GET("/ping", func(c *gin.Context) { c.JSON(200, gin.H{"message": "pong"}) })
+	r.GET("/users", app.returnGoods)
+	r.NoRoute(func(c *gin.Context) {
+		render(c, gin.H{"payload": "not found"})
+	})
+
+}
+
+func (app *App) returnGoods(c *gin.Context) {
+	users := []*User{}
+	app.db.Find(&users)
+	render(c, gin.H{"payload": users})
+}
+
+func render(c *gin.Context, data gin.H) {
+
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Credentials", "true")
+	c.Header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+	c.Header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+
+	c.JSON(http.StatusOK, data["payload"])
+}
